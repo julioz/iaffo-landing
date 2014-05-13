51c40f48cf4cb429b7a517a4d6eac3c35db1084b
julioz <julioz92@gmail.com>
2014-05-13 19:05:03 -0300

    Adding device attribute to database.

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
-$(document).ready(function() {    
-    
+$(document).ready(function() {
     function validateEmail(email) { 
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
@@ -43,6 +42,14 @@ $(document).ready(function() {
         
         subscriber.set("name", name);
         subscriber.set("email", email);
+        
+        if (isMobile.Android()) {
+            subscriber.set("device", "android");
+        } else if(isMobile.iOS()) {
+            subscriber.set("device", "iOS");
+        } else {
+            subscriber.set("device", "desktop");
+        }
      
         subscriber.save(null, {
             success: function(subscriber) {
@@ -53,4 +60,31 @@ $(document).ready(function() {
             }
         });
     });
+    
+    
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+
+    };
+    
+    function isMobileDevice() {
+        return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+    };
 });
