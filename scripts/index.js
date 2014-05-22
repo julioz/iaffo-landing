7cc4c39eca485a98c9e3acd9d849912cea6edc33
julioz <julioz92@gmail.com>
2014-05-22 19:18:37 -0300

    Replaces logo with high quality one

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,4 @@
 $(document).ready(function() {
-    /*var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-51066928-1']);
-      _gaq.push(['_trackPageview']);
-
-      
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    */  
-
     function validateEmail(email) { 
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
